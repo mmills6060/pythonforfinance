676b155b441690b537a2c66133c874ed066e697c
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2022-02-02 10:47:41 -0500

    Add files via upload

---
+++ scripts/pullmarketdata.py
@@ -0,0 +1,13 @@
+from yahoo_fin.stock_info import *
+
+stock=input("Enter a stock ticker symbol: ")
+print(stock)
+
+
+print(get_live_price(stock))
+print(get_live_price(stock))
+print(get_live_price(stock))
+print(get_live_price(stock))
+print(get_live_price(stock))
+print(get_live_price(stock))
+
